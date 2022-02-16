a27105d04a1744a79a6c19f50900473362cfddf2
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-02-16 17:55:58 -0500

    Add test for login link

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -2,29 +2,45 @@ require 'rails_helper'
 
 RSpec.describe 'home page', type: :feature do
   it 'has a link to return to the landing page' do
-    visit ('/')
+    visit('/')
     expect(page).to have_link('Return to home')
     expect(page).to have_content('Viewing Party')
   end
-  
+
   it 'has a create user button' do
-    visit ('/')
+    visit('/')
     expect(page).to have_button('Create a New User')
-    
+
     click_button('Create a New User')
     expect(current_path).to eq('/register')
   end
-  
-  it 'has links to user dashboards' do
-    user1 = User.create!(name: 'Joe Schmoe', email: 'jschmoe@mail.com', password: '1234mac', password_confirmation: '1234mac')
-    user2 = User.create!(name: 'Jane Schmoe', email: 'janeschmoe@mail.com', password: 'horseradish', password_confirmation: 'horseradish')
-    user3 = User.create!(name: 'Moe Schmoe', email: 'moeshmoe@mail.com', password: 'remington', password_confirmation: 'remington')
-    visit ('/')
 
-    within(".existing-users") do
+  it 'has links to user dashboards' do
+    user1 = User.create!(name: 'Joe Schmoe', email: 'jschmoe@mail.com', password: '1234mac',
+                         password_confirmation: '1234mac')
+    user2 = User.create!(name: 'Jane Schmoe', email: 'janeschmoe@mail.com', password: 'horseradish',
+                         password_confirmation: 'horseradish')
+    user3 = User.create!(name: 'Moe Schmoe', email: 'moeshmoe@mail.com', password: 'remington',
+                         password_confirmation: 'remington')
+    visit('/')
+
+    within('.existing-users') do
       expect(page).to have_link("#{user1.email}'s Dashboard")
     end
     click_link "#{user1.email}'s Dashboard"
     expect(current_path).to eq("/users/#{user1.id}")
+  end
+
+  it 'has a login link' do
+    user1 = User.create!(name: 'Joe Schmoe', email: 'jschmoe@mail.com', password: '1234mac',
+                         password_confirmation: '1234mac')
+    visit '/'
+    expect(page).to have_link('Login')
+      save_and_open_page
+    click_link 'Login'
+
+    expect(current_path).to eq('/login')
+
+
   end
 end
