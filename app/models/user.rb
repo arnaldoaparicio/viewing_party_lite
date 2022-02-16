3bf45b33e6f500feb2f4dd34a1bc06768b941194
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-02-16 15:37:18 -0500

    Add has_secure_password and validation of password_digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   validates :email, uniqueness: true
-  
+  validates_presence_of :password_digest
   has_many :user_parties
-  has_many :viewing_parties, through: :user_parties  
+  has_many :viewing_parties, through: :user_parties
+
+  has_secure_password
 end
