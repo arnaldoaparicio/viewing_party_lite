f8e117b343f8806f2fe3a5b5ba6d693f6e4af8b4
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-02-17 23:58:14 -0500

    Add test for a visible logout link

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -24,23 +24,40 @@ RSpec.describe 'home page', type: :feature do
                          password_confirmation: 'remington')
     visit('/')
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user1)
-    
+
     within('.existing-users') do
       expect(page).to have_link("#{user1.email}'s Dashboard")
     end
-    
+
     click_link "#{user1.email}'s Dashboard"
     expect(current_path).to eq('/dashboard')
   end
-  
+
   it 'has a login link' do
     user1 = User.create!(name: 'Joe Schmoe', email: 'jschmoe@mail.com', password: '1234mac',
-      password_confirmation: '1234mac')
-      visit '/'
-      # allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user1)
+                         password_confirmation: '1234mac')
+    visit '/'
     expect(page).to have_link('Login')
     click_link 'Login'
 
     expect(current_path).to eq('/login')
+  end
+
+  it 'has a logout link' do
+    user1 = User.create!(name: 'Joe Schmoe', email: 'jschmoe@mail.com', password: '1234mac',
+                         password_confirmation: '1234mac')
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user1)
+    visit '/'
+    expect(page).to have_content('Logout')
+  end
+  
+  xit 'logs out of current user' do
+    user1 = User.create!(name: 'Joe Schmoe', email: 'jschmoe@mail.com', password: '1234mac',
+                         password_confirmation: '1234mac')
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user1)
+    visit '/'
+    expect(page).to have_content('Logout')
+    click_link 'Logout'
+    expect(page).to have_content('Login')
   end
 end
