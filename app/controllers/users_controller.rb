edaa859e2f63524ec4c4fb6647391e28cd890e4c
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-02-17 23:48:03 -0500

    Refactor methods including adding a destroy method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,20 @@
 class UsersController < ApplicationController
   def show
+    if !current_user.nil?
     @user = User.find(current_user.id)
+    else 
+      flash[:alert] = 'You must be logged in or registered to asccess dashboard'
+    end
   end
 
-  def new
-  end
+  def new; end
 
   def create
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
       flash[:success] = 'User created!'
-      redirect_to user_path(@user)
+      redirect_to '/dashboard'
     elsif params[:password] != params[:password_confirmation]
       flash[:alert] = 'Password and password confirmation do not match'
       redirect_to '/register'
@@ -22,22 +25,28 @@ class UsersController < ApplicationController
   end
 
   def discover
-    @user = User.find(params[:id])
+    @user = User.find(current_user.id)
   end
 
-  def login_form
-  end
+  def login_form; end
 
   def login_user
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       flash[:success] = "Welcome, #{user.name}!"
-      redirect_to user_path(user)
+      redirect_to '/dashboard'
     else
       flash[:error] = 'Sorry, your credentials are bad.'
       render '/users/login_form'
     end
+  end
+
+  # user logout
+  def destroy
+    reset_session
+    current_user = nil
+    redirect_to '/'
   end
 
   private
