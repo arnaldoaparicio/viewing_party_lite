22bf030c21fa3598784414d98daf246e292c545e
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-02-17 23:47:19 -0500

    Refactor details method

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,12 +4,12 @@ class MoviesController < ApplicationController
 
   def results
     @m_data = MovieFacade.details(params[:query])
-    @user = User.find(params[:id])
+    @user = User.find(current_user.id)
     render '/users/movies'
   end
 
   def details
-    @user = User.find(params[:id])
+    # @user = User.find(current_user.id)
     @movie = MovieFacade.movie_data(params[:movie_id])
     @credits = MovieFacade.credits(params[:movie_id])
     @movie_reviews = MovieFacade.reviews(params[:movie_id])
