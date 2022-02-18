67b3d0d43b13facfbc437656a2dd0ce7b2ca180e
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-02-17 23:57:26 -0500

    Change visit links

--- spec/features/discover_movies_spec.rb
+++ spec/features/discover_movies_spec.rb
@@ -2,27 +2,29 @@ require 'rails_helper'
 
 RSpec.describe "Discover movies page", type: :feature do
 
-  it "has button for listing top-rated movies", :vcr do 
+  it 'has button for listing top-rated movies', :vcr do 
     user1 = User.create!(name: "Joe Schmoe", email: "joeschmoe@mail.com", password: '1234mac', password_confirmation: '1234mac')
-    visit "/users/#{user1.id}/discover"
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user1)
+    visit "/discover"
 
     expect(page).to have_button("Find Top Rated Movies")
     
     click_button("Find Top Rated Movies")
     
-    expect(current_path).to eq("/users/#{user1.id}/movies")
+    expect(current_path).to eq("/movies")
   end
 
   it "has button for searching for movies by keyword", :vcr do 
     user1 = User.create!(name: "Joe Schmoe", email: "joeschmoe@mail.com", password: '1234mac', password_confirmation: '1234mac')
-    visit "/users/#{user1.id}/discover"
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user1)
+    visit "/discover"
 
     expect(page).to have_button("Find Movies")
     
     fill_in :query, with: "Spider"
     click_button "Find Movies"
     
-    expect(current_path).to eq("/users/#{user1.id}/movies")
+    expect(current_path).to eq("/movies")
   end
 
 end
