a787865218399500a9e0ab0ca81a312973fd3d40
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-02-19 19:23:12 -0500

    Fix typing error

--- app/controllers/viewing_party_controller.rb
+++ app/controllers/viewing_party_controller.rb
@@ -15,38 +15,30 @@ class ViewingPartyController < ApplicationController
     @users = User.where.not(id: params[:id])
   end
 
-  def viewing_party_create
-  end
-
   def create
-    if !current_user.nil?
-      response = conn.get("movie/#{params[:movie_id]}") do |request|
-        request.params['api_key'] = ENV['movies_api_key']
-      end
+    response = conn.get("movie/#{params[:movie_id]}") do |request|
+      request.params['api_key'] = ENV['movies_api_key']
+    end
 
-      @movie = JSON.parse(response.body, symbolize_names: true)
+    @movie = JSON.parse(response.body, symbolize_names: true)
 
-      user = User.find(params[:id])
+    user = User.find(params[:id])
 
-      @viewing_party = ViewingParty.create(party_params)
-      users = User.find(params[:user_ids])
-      @viewing_party.user_parties.create(user: user, host: true)
-      users.each do |u|
-        @viewing_party.user_parties.create(user: u, host: false)
-        @viewing_party.movie_title = @movie[:original_title]
-        @viewing_party.poster_path = @movie[:poster_path]
-      end
+    @viewing_party = ViewingParty.create(party_params)
+    users = User.find(params[:user_ids])
+    @viewing_party.user_parties.create(user: user, host: true)
+    users.each do |u|
+      @viewing_party.user_parties.create(user: u, host: false)
+      @viewing_party.movie_title = @movie[:original_title]
+      @viewing_party.poster_path = @movie[:poster_path]
+    end
 
-      if @viewing_party.save
-        flash[:success] = 'Party created!'
-        redirect_to '/dashboard'
-      else
-        flash[:alert] = 'Could not create party, please try again.'
-        redirect_to "/movies/#{params[:movie_id]}/viewing-party/new"
-      end
+    if @viewing_party.save
+      flash[:success] = 'Party created!'
+      redirect_to '/dashboard'
     else
-        flash[:alert] = 'You must be logged in or registered to create a viewing party'
-        redirect_to "/movies/#{params[:movie_id]}"
+      flash[:alert] = 'Could not create party, please try again.'
+      redirect_to "/movies/#{params[:movie_id]}/viewing-party/new"
     end
   end
 
